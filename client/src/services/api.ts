fad17d59d8d1a59d8acc3ce1eb323ee0ff7de5f5
cndrmhrdk <cndrmhrdka@gmail.com>
2026-07-13 11:08:39 +0700

    update api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://odyssey-production-3597.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
