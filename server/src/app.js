4a6ffda944161d92e0635bcba80dd11df852520d
cndrmhrdk <cndrmhrdka@gmail.com>
2026-07-13 08:55:54 +0700

    delete swagger

--- server/src/app.js
+++ server/src/app.js
@@ -6,19 +6,11 @@ const app = express();
 const routes = require("./routes");
 const errorHandler = require("./middlewares/error.middleware");
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./docs/swagger");
-
-// middleware
 app.use(cors());
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// routes
 app.use("/", routes);
 
-// error middleware
 app.use(errorHandler);
 
 module.exports = app;
